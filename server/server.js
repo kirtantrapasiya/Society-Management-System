a0fd410dc3ddce0d823e28d6ddc87714f042e4bc
Kirtan Trapasiya <trapasiyakt2399@gmail.com>
2025-10-02 16:53:44 +0530

    add

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: process.env.CLIENT_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
